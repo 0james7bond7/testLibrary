2f635004ef4d2f8ce61a50213e33227a792cc2ea
James Bond <james@Jamess-MacBook-Pro.local>
2018-02-07 17:32:50 +0200

    first commadsaasdit

--- testPodLib/Classes/ReplaceMe.swift
+++ testPodLib/Classes/ReplaceMe.swift
@@ -6,10 +6,17 @@
 //
 //
 
+import UIKit
 import Foundation
 
 public class testLibClass {
-    public static func testLibFunc() -> Bool {
+    public static func testLibFunc2() -> Bool {
         return true
+    }
+    
+    public static func openInitialView() -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "testViewConroller") as! ViewController
+        return vc
     }
 }
